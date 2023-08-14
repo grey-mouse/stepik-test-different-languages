34328c091d5ecfde958ae20b513fa176597004b4
grey-mouse <nadezhda.yarygina@gmail.com>
2023-08-14 14:48:07 +0300

    add comments

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ https://stepik.org/lesson/237240/step/10?unit=209628
 
 To run tests use optional parameters "browser_name" ('chrome' by default) and "language" ('en' by default).
 
-Example: 
+Examples:
+ 
 pytest --browser_name=chrome --language=es test_items.py 
+
+pytest --language=fr test_items.py
--- test_items.py
+++ test_items.py
@@ -5,6 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_user_should_see_add_to_basket_button(browser):
 	browser.get(link)
-	time.sleep(10)
+	#for visual checking that language has changed
+	#time.sleep(30)
 	button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
 	assert button, "Should be present 'add to basket' button"
